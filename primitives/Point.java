b16b32a51227aa151060ac795f82dacc4b24e5fd
DVORA-AZARKOVICH <azerkovi@g.jct.ac.il>
2022-03-02 23:31:42 +0200

    ex 1

--- primitives/Point.java
+++ primitives/Point.java
@@ -40,7 +40,7 @@ public class Point {
         Vector newVector = new Vector(x,y,z);
         return newVector;
     }
-
+    //add function
     public 	Point add (Point p2){
         double x = p2.dpoint.d1+this.dpoint.d1;
         double y = p2.dpoint.d2+this.dpoint.d2;
